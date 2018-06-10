5378ec0c9fc5a4fac0a212fe0637f47c12be217f
Ankur Mehta <ankurmehta@yahoo.com>
2018-06-10 18:09:42 +0530

    tried writing to google sheet, but getting auth error

--- hello.py
+++ hello.py
@@ -21,10 +21,20 @@ RANGE_NAME = 'Class Data!A:E'
 result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
                                              range=RANGE_NAME).execute()
 values = result.get('values', [])
+count=0
 if not values:
     print('No data found.')
 else:
-    print('Name, Major:')
+    #print('Name, Major:')
     for row in values:
         # Print columns A and E, which correspond to indices 0 and 4.
-        print('%s, %s, %s, %s, %s' % (row[0], row[1], row[2], row[3], row[4]))
+        #print('%s, %s, %s, %s, %s' % (row[0], row[1], row[2], row[3], row[4]))
+        if row[4]=="E":
+            count += 1
+    print("count of E = ", count)
+dest_spread = '1xJC7GewEHCS6oqJrbcJ8i5NDyQ6MZQqrI7JV-GgX-pw'
+dest_range = 'Sheet1!A1'
+value_input_option = 'RAW'
+request = service.spreadsheets().values().update(spreadsheetId=dest_spread,range=dest_range, valueInputOption='RAW',body=values)
+response = request.execute()
+print(response)
