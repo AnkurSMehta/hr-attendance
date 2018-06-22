62ccad164b42a53a3f8e780922ba41b7104e92e2
Ankur Mehta <ankurmehta@yahoo.com>
2018-06-22 15:46:27 +0530

    compile from monthly files, check 2 errors and write df to excel;

--- Compile.py
+++ Compile.py
@@ -1,5 +1,47 @@
 # this compiles the data from individual sheets into a master attendance sheet
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import xlrd
+import xlwt
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('gspreadhw.json', scope)
+client = gspread.authorize(creds)
+
+samplefile="General_generalM.xlsx"
+sheet=client.open(samplefile).sheet1
+list_of_hashes=sheet.get_all_records()
+Col_labels=['Dep','Loc','Act','Name','ID','Mon','Days_Attendance','Total_Sessions']
+testdf=pd.DataFrame(list_of_hashes, index=None, columns=Col_labels)
+
+sample2="General_VaniM.xlsx"
+sheet2=client.open(sample2).sheet1
+list_of_hash2=sheet2.get_all_records()
+test2=pd.DataFrame(list_of_hash2, columns=Col_labels)
+
+error_log=[]
+for i, row in testdf.iterrows():
+    if (row['Days_Attendance'] > row['Total_Sessions']):
+        print("Too many Seva Days ", i),
+        error_log.append(("Too Many",i))
+    elif (row['Days_Attendance']==""):
+        print("Seva Days Blank ", i),
+        error_log.append(("Blank",i))
+error_log
+
+attend_master_df=pd.DataFrame(columns=Col_labels)
+attend_master_df=attend_master_df.append(testdf)
+attend_master_df=attend_master_df.append(test2)
+attend_master_df.shape
+
+writer = pd.ExcelWriter("Attendance_Master_Compile.xlsx")
+attend_master_df.to_excel(writer)
+writer.save()
+
+
+'''
 import Validate
 import pandas as pd 
 
@@ -34,6 +76,6 @@ def calculate_total_days_per_ID(entry_form,month):
 def write_to_master(DataFrame):
     # write the dataframe to google drive - using sheets API
 
-
+'''
 
 
